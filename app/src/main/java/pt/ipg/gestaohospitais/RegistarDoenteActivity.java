90993562ac252f70fe7e79d17bb90f0d21d0838a
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2020-04-27 17:58:12 +0100

    Validações dos campos no registo de doentes

--- app/src/main/java/pt/ipg/gestaohospitais/RegistarDoenteActivity.java
+++ app/src/main/java/pt/ipg/gestaohospitais/RegistarDoenteActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistarDoenteActivity extends AppCompatActivity {
 
@@ -27,6 +28,52 @@ public class RegistarDoenteActivity extends AppCompatActivity {
     }
 
     public void openSintomasActivity(){
+
+        EditText editTextNomeUtente = (EditText) findViewById(R.id.editTextNomeUtente);
+        String nomeUtente = editTextNomeUtente.getText().toString();
+
+        if(nomeUtente.length() == 0){
+            editTextNomeUtente.setError("Introduza o nome");
+            editTextNomeUtente.requestFocus();
+            return;
+        }
+
+        EditText editTextNumeroUtente = (EditText) findViewById(R.id.editTextNumeroUtente);
+        String numeroUtente = editTextNumeroUtente.getText().toString();
+
+        if(numeroUtente.length() == 0){
+            editTextNumeroUtente.setError("Introduza o numero de utente");
+            editTextNumeroUtente.requestFocus();
+            return;
+        }
+
+        EditText editTextIdade = (EditText) findViewById(R.id.editTextIdade);
+        String idade = editTextIdade.getText().toString();
+
+        if(idade.length() == 0){
+            editTextIdade.setError("Introduza a idade");
+            editTextIdade.requestFocus();
+            return;
+        }
+
+        EditText editTextMorada = (EditText) findViewById(R.id.editTextMorada);
+        String morada = editTextMorada.getText().toString();
+
+        if(morada.length() == 0){
+            editTextMorada.setError("Introduza a morada");
+            editTextMorada.requestFocus();
+            return;
+        }
+
+        EditText editTextTelefone = (EditText) findViewById(R.id.editTextTelefone);
+        String telefone = editTextTelefone.getText().toString();
+
+        if(telefone.length() != 9){
+            editTextTelefone.setError("Introduza um numero de telefone valido");
+            editTextTelefone.requestFocus();
+            return;
+        }
+
         Intent intent = new Intent(this, SintomasActivity.class);
         startActivity(intent);
     }
