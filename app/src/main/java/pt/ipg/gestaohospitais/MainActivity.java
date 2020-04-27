b07d30ebfa75750b7a8d7f3f86df91f3e6a712ce
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2020-04-27 14:54:19 +0100

    Criado botão Registar Doente na MainActivity, abrindo assim a RegistarDoenteActivity

--- app/src/main/java/pt/ipg/gestaohospitais/MainActivity.java
+++ app/src/main/java/pt/ipg/gestaohospitais/MainActivity.java
@@ -2,13 +2,34 @@ package pt.ipg.gestaohospitais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.FontsContract;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonRegistarDoente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonRegistarDoente = (Button) findViewById(R.id.buttonRegistarDoente);
+        buttonRegistarDoente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegistarDoenteActivity();
+            }
+        });
+
     }
+
+    public void openRegistarDoenteActivity(){
+        Intent intent = new Intent(this, RegistarDoenteActivity.class);
+        startActivity(intent);
+    }
+
 }
