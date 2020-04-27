a34fe6d6822d3b687f78b66d98d026763b08854b
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2020-04-27 17:10:49 +0100

    Criada a SintomasActivity

--- app/src/main/java/pt/ipg/gestaohospitais/RegistarDoenteActivity.java
+++ app/src/main/java/pt/ipg/gestaohospitais/RegistarDoenteActivity.java
@@ -2,13 +2,32 @@ package pt.ipg.gestaohospitais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistarDoenteActivity extends AppCompatActivity {
+
+    private Button buttonAbrirSintomas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registar_doente);
+
+        buttonAbrirSintomas = (Button) findViewById(R.id.buttonAbrirSintomas);
+        buttonAbrirSintomas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSintomasActivity();
+            }
+        });
+
+    }
+
+    public void openSintomasActivity(){
+        Intent intent = new Intent(this, SintomasActivity.class);
+        startActivity(intent);
     }
 }
