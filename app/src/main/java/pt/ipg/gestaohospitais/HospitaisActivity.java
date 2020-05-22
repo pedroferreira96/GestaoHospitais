45285e22e186a32c0b328d5321b326a33ed38a84
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2020-05-22 16:17:03 +0100

    Criação da HospitaisActivity e alterações na SintomasActivity

---
+++ app/src/main/java/pt/ipg/gestaohospitais/HospitaisActivity.java
@@ -0,0 +1,14 @@
+package pt.ipg.gestaohospitais;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HospitaisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospitais);
+    }
+}
