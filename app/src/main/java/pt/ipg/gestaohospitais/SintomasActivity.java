96aee482b6026fc9d446c768d927045f3d9d6657
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2020-05-22 16:35:39 +0100

    Correções na atividade dos sintomas e botão funcional para passar da atividade dos sintomas para os hospitais

--- app/src/main/java/pt/ipg/gestaohospitais/SintomasActivity.java
+++ app/src/main/java/pt/ipg/gestaohospitais/SintomasActivity.java
@@ -2,13 +2,31 @@ package pt.ipg.gestaohospitais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SintomasActivity extends AppCompatActivity {
+
+    private Button buttonAbrirHospitais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sintomas);
+
+        buttonAbrirHospitais = (Button) findViewById(R.id.buttonAbrirHospitais);
+        buttonAbrirHospitais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHospitaisActivity();
+            }
+        });
+    }
+
+    public void openHospitaisActivity(){
+        Intent intent = new Intent(this, HospitaisActivity.class);
+        startActivity(intent);
     }
 }
