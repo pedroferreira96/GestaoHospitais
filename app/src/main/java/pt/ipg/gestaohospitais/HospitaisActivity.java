0fe17d60eee627de62e4f45c6d35a6bb3f0d1284
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2020-06-12 15:07:51 +0100

    Botão a abrir atividade inicial com mensagem de sucesso

--- app/src/main/java/pt/ipg/gestaohospitais/HospitaisActivity.java
+++ app/src/main/java/pt/ipg/gestaohospitais/HospitaisActivity.java
@@ -2,13 +2,34 @@ package pt.ipg.gestaohospitais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HospitaisActivity extends AppCompatActivity {
+
+    private Button buttonConfirmarRegisto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospitais);
+
+        buttonConfirmarRegisto = (Button) findViewById(R.id.buttonConfirmarRegisto);
+        buttonConfirmarRegisto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HospitaisActivity.this, "Registo Guardado", Toast.LENGTH_SHORT).show();
+                openMainActivity();
+            }
+        });
+
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
