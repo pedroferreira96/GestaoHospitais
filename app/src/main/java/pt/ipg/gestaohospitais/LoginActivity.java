d0d1276d444f34bd5c52a5faf298bb591938e440
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2020-06-19 16:02:43 +0100

    Login a funcionar corretamente

--- app/src/main/java/pt/ipg/gestaohospitais/LoginActivity.java
+++ app/src/main/java/pt/ipg/gestaohospitais/LoginActivity.java
@@ -2,13 +2,43 @@ package pt.ipg.gestaohospitais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button login;
+    private String userName;
+    private String userPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.editTextUsername);
+        password = (EditText) findViewById(R.id.editTextPassword);
+        login = (Button) findViewById(R.id.buttonLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(username.getText().toString(), password.getText().toString());
+            }
+        });
+    }
+
+
+    public void validate(String userName, String userPassword) {
+        if ((userName.equals("admin")) && (userPassword.equals("covid"))) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
