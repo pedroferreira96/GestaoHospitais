8dfd845cb43fcb080044b93db51cd02ebdf31734
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2020-06-23 16:11:19 +0100

    Radio Buttons a funcionar na atividade hospitais

--- app/src/main/java/pt/ipg/gestaohospitais/HospitaisActivity.java
+++ app/src/main/java/pt/ipg/gestaohospitais/HospitaisActivity.java
@@ -6,21 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class HospitaisActivity extends AppCompatActivity {
 
     private Button buttonConfirmarRegisto;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hospitais);
 
+        radioGroup = findViewById(R.id.radioGroup);
+
         buttonConfirmarRegisto = (Button) findViewById(R.id.buttonConfirmarRegisto);
         buttonConfirmarRegisto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                int radioId = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(radioId);
+
                 Toast.makeText(HospitaisActivity.this, "Registo Guardado", Toast.LENGTH_SHORT).show();
                 openMainActivity();
             }
