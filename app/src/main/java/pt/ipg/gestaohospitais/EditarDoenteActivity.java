ca7d7c6300df28b9aa297c7cbd843020dea1a7f2
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2020-06-26 21:36:18 +0100

    Criada atividade de editar doente e feito o respetivo layout

---
+++ app/src/main/java/pt/ipg/gestaohospitais/EditarDoenteActivity.java
@@ -0,0 +1,14 @@
+package pt.ipg.gestaohospitais;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditarDoenteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_doente);
+    }
+}
